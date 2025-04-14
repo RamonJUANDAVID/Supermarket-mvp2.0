4c4ee784e9984961d2d83ca3f84bbceeb01bc2ee
RamonJUANDAVID <jdramon30@gmail.com>
2025-04-13 20:46:01 -0500

    Primer commit

---
+++ Views/IPayModeView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Views
+{
+    internal interface IPayModeView
+    {
+        string PayModeId { get; set; }
+        string PayModeName { get; set; }
+        string PayModeObservation { get; set; }
+
+        string SearchValue { get; set; }
+        bool isEdit { get; set; }
+        bool isSuccessful { get; set; }
+        string Message { get; set; }
+
+        event EventHandler SearchEvent;
+        event EventHandler AddnNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvente;
+        event EventHandler CancelEvent;
+
+        void SetPayModeListBildingSource(BindingSource payModelist); 
+        void Show();
+
+    }
+}
